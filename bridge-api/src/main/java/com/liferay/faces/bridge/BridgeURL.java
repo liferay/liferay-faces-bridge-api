606006704db432507db12a266c4f844b81522e6a
Neil Griffin <neil.griffin.scm@gmail.com>
2016-04-16 17:43:08 -0400

    FACES-2637 Move BridgeURL to com.liferay.faces.bridge and simplify the API by moving implementation details to the bridge implementation

---
+++ bridge-api/src/main/java/com/liferay/faces/bridge/BridgeURL.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.bridge;
+
+import java.util.Map;
+
+
+/**
+ * This interface represents a bridge URL, meaning a URL that has convenience methods for representing URLs according to
+ * Section 6.1.3.1 of the Bridge Spec.
+ *
+ * @author  Neil Griffin
+ */
+public interface BridgeURL {
+
+	/**
+	 * Returns the first value of the underlying {@link BridgeURL#getParameterMap()} with the specified <code>
+	 * name</code>.
+	 */
+	public String getParameter(String name);
+
+	/**
+	 * Returns a mutable {@link Map} representing the URL parameters.
+	 */
+	public Map<String, String[]> getParameterMap();
+
+	/**
+	 * If the URL targets a Faces view, then returns the viewId. Otherwise, returns null.
+	 *
+	 * @return  if the URL targets a Faces View, returns the viewId. Otherwise <code>null</code>.
+	 */
+	public String getViewId();
+
+	/**
+	 * Sets the <code>value</code> of the underlying {@link BridgeURL#getParameterMap()} according to the specified
+	 * <code>name</code>.
+	 */
+	public void setParameter(String name, String value);
+
+	/**
+	 * Sets the <code>value</code> of the underlying {@link BridgeURL#getParameterMap()} according to the specified
+	 * <code>name</code>.
+	 */
+	public void setParameter(String name, String[] value);
+
+	/**
+	 * Removes the entry of the underlying {@link BridgeURL#getParameterMap()} according to the specified <code>
+	 * name</code>.
+	 *
+	 * @return  the first value of the underlying {@link BridgeURL#getParameterMap()} with the specified <code>
+	 *          name</code>.
+	 */
+	public String removeParameter(String name);
+
+	/**
+	 * Returns a string-based representation of the URL.
+	 */
+	@Override
+	public String toString();
+}
