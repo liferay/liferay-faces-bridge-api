39aa8a664108fe2a2aaf2778307a075a9b2bb353
Neil Griffin <neil.griffin.scm@gmail.com>
2016-06-18 09:05:03 -0400

    FACES-2552 Add javax.portlet.faces.BridgeFactory and javax.portlet.faces.BridgeWrapper to the Bridge API

---
+++ bridge-api/src/main/java/com/liferay/faces/bridge/BridgeWrapper.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.bridge;
+
+import javax.faces.FacesWrapper;
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.EventRequest;
+import javax.portlet.EventResponse;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+import javax.portlet.faces.Bridge;
+import javax.portlet.faces.BridgeDefaultViewNotSpecifiedException;
+import javax.portlet.faces.BridgeException;
+import javax.portlet.faces.BridgeUninitializedException;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public abstract class BridgeWrapper implements Bridge, FacesWrapper<Bridge> {
+
+	@Override
+	public abstract Bridge getWrapped();
+
+	@Override
+	public void destroy() {
+		getWrapped().destroy();
+	}
+
+	@Override
+	public void doFacesRequest(ActionRequest actionRequest, ActionResponse actionResponse)
+		throws BridgeDefaultViewNotSpecifiedException, BridgeUninitializedException, BridgeException {
+		getWrapped().doFacesRequest(actionRequest, actionResponse);
+	}
+
+	@Override
+	public void doFacesRequest(EventRequest eventRequest, EventResponse eventResponse)
+		throws BridgeUninitializedException, BridgeException {
+		getWrapped().doFacesRequest(eventRequest, eventResponse);
+	}
+
+	@Override
+	public void doFacesRequest(RenderRequest renderRequest, RenderResponse renderResponse)
+		throws BridgeDefaultViewNotSpecifiedException, BridgeUninitializedException, BridgeException {
+		getWrapped().doFacesRequest(renderRequest, renderResponse);
+	}
+
+	@Override
+	public void doFacesRequest(ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws BridgeUninitializedException, BridgeException {
+		getWrapped().doFacesRequest(resourceRequest, resourceResponse);
+	}
+}
