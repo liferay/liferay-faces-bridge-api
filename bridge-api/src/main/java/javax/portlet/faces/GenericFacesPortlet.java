6fe3dda984d4e4f782cd620beaa4e955970ccee9
Neil Griffin <neil.griffin.scm@gmail.com>
2016-06-18 20:05:24 -0400

    FACES-2768 GenericFacesPortlet does not define the BRIDGE_AUTO_DISPATCH_EVENTS string constant

--- bridge-api/src/main/java/javax/portlet/faces/GenericFacesPortlet.java
+++ bridge-api/src/main/java/javax/portlet/faces/GenericFacesPortlet.java
@@ -48,6 +48,7 @@ import javax.portlet.ResourceResponse;
 public class GenericFacesPortlet extends GenericPortlet {
 
 	// Public Constants
+	public static final String BRIDGE_AUTO_DISPATCH_EVENTS = "javax.portlet.faces.autoDispatchEvents";
 	public static final String BRIDGE_CLASS = "javax.portlet.faces.BridgeImplClass";
 
 	/**
@@ -276,7 +277,7 @@ public class GenericFacesPortlet extends GenericPortlet {
 		}
 
 		// Determine whether or not all events should be auto-dispatched.
-		String initParamValue = portletConfig.getInitParameter("javax.portlet.faces.autoDispatchEvents");
+		String initParamValue = portletConfig.getInitParameter(BRIDGE_AUTO_DISPATCH_EVENTS);
 
 		// TCK TestPage034: isAutoDispatchEventsSetTest
 		autoDispatchEvents = ((initParamValue == null) || Boolean.parseBoolean(initParamValue));
