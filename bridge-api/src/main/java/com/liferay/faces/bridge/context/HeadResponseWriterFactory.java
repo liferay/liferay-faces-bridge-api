e4431d14d1e64478c6ed67df165ace9f8b6091f6
Neil Griffin <neil.griffin.scm@gmail.com>
2016-06-29 17:55:55 -0400

    FACES-2786 Remove the com.liferay.faces.bridge.context.HeadResponseWriter marker class

--- bridge-api/src/main/java/com/liferay/faces/bridge/context/HeadResponseWriterFactory.java
+++ bridge-api/src/main/java/com/liferay/faces/bridge/context/HeadResponseWriterFactory.java
@@ -27,10 +27,10 @@ import javax.portlet.faces.BridgeFactoryFinder;
 public abstract class HeadResponseWriterFactory implements FacesWrapper<HeadResponseWriterFactory> {
 
 	/**
-	 * Returns an instance of {@link HeadResponseWriter} from the {@link HeadResponseWriterFactory} found by the {@link
+	 * Returns an instance of {@link ResponseWriter} from the {@link HeadResponseWriterFactory} found by the {@link
 	 * BridgeFactoryFinder}.
 	 */
-	public static HeadResponseWriter getHeadResponseWriterInstance(ResponseWriter responseWriter,
+	public static ResponseWriter getHeadResponseWriterInstance(ResponseWriter responseWriter,
 		PortletResponse portletResponse) {
 
 		HeadResponseWriterFactory headResponseWriterFactory = (HeadResponseWriterFactory) BridgeFactoryFinder
@@ -39,6 +39,6 @@ public abstract class HeadResponseWriterFactory implements FacesWrapper<HeadResp
 		return headResponseWriterFactory.getHeadResponseWriter(responseWriter, portletResponse);
 	}
 
-	public abstract HeadResponseWriter getHeadResponseWriter(ResponseWriter responseWriter,
+	public abstract ResponseWriter getHeadResponseWriter(ResponseWriter responseWriter,
 		PortletResponse portletResponse);
 }
