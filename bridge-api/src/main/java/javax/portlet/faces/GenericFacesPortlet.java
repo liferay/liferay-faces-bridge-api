liferay/liferay-faces-bridge-api

commit af62b3fd16d7540c2687dce6c06a75d47de72be2

FACES-2695 Upgrade to version 3.0 of the Portlet API (fixed deprecation warnings)

bridge-api/src/main/java/javax/portlet/faces/GenericFacesPortlet.java
@@ -319,13 +319,13 @@ public class GenericFacesPortlet extends GenericPortlet {
 	public Bridge getFacesBridge(PortletRequest portletRequest, PortletResponse portletResponse)
 		throws PortletException {
 
-		String viewId = portletRequest.getParameter(Bridge.FACES_VIEW_ID_PARAMETER);
+		String viewId = portletRequest.getRenderParameters().getValue(Bridge.FACES_VIEW_ID_PARAMETER);
 
 		if (viewId != null) {
 			portletRequest.setAttribute(Bridge.VIEW_ID, viewId);
 		}
 		else {
-			String viewPath = portletRequest.getParameter(Bridge.FACES_VIEW_PATH_PARAMETER);
+			String viewPath = portletRequest.getRenderParameters().getValue(Bridge.FACES_VIEW_PATH_PARAMETER);
 
 			if (viewPath != null) {
 				portletRequest.setAttribute(Bridge.VIEW_PATH, viewPath);
@@ -555,7 +555,7 @@ public class GenericFacesPortlet extends GenericPortlet {
 		IOException {
 
 		String autoNonFacesViewDispatch = getInitParameter("javax.portlet.faces.automaticNonFacesViewDispatching");
-		String nonFacesTargetPath = renderRequest.getParameter(Bridge.NONFACES_TARGET_PATH_PARAMETER);
+		String nonFacesTargetPath = renderRequest.getRenderParameters().getValue(Bridge.NONFACES_TARGET_PATH_PARAMETER);
 
 		if ((autoNonFacesViewDispatch != null) && autoNonFacesViewDispatch.equalsIgnoreCase("true") &&
 				(nonFacesTargetPath != null)) {

commit 03b7eaf813414aa6f1ae1e8d9041b31b376ddd6b

FACES-2695 Upgrade to version 3.0 of the Portlet API (fixed deprecation warnings - correct error introduced in af62b3fd16d7540c2687dce6c06a75d47de72be2)

bridge-api/src/main/java/javax/portlet/faces/GenericFacesPortlet.java
@@ -34,6 +34,7 @@ import javax.portlet.HeaderResponse;
 import javax.portlet.PortletConfig;
 import javax.portlet.PortletContext;
 import javax.portlet.PortletException;
+import javax.portlet.PortletParameters;
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletRequestDispatcher;
 import javax.portlet.PortletResponse;
@@ -324,13 +325,13 @@ public class GenericFacesPortlet extends GenericPortlet {
 	public Bridge getFacesBridge(PortletRequest portletRequest, PortletResponse portletResponse)
 		throws PortletException {
 
-		String viewId = portletRequest.getRenderParameters().getValue(Bridge.FACES_VIEW_ID_PARAMETER);
+		String viewId = getPortletParameters(portletRequest).getValue(Bridge.FACES_VIEW_ID_PARAMETER);
 
 		if (viewId != null) {
 			portletRequest.setAttribute(Bridge.VIEW_ID, viewId);
 		}
 		else {
-			String viewPath = portletRequest.getRenderParameters().getValue(Bridge.FACES_VIEW_PATH_PARAMETER);
+			String viewPath = getPortletParameters(portletRequest).getValue(Bridge.FACES_VIEW_PATH_PARAMETER);
 
 			if (viewPath != null) {
 				portletRequest.setAttribute(Bridge.VIEW_PATH, viewPath);
@@ -716,6 +717,16 @@ public class GenericFacesPortlet extends GenericPortlet {
 		}
 
 		return bridgeService;
+	}
+
+	private PortletParameters getPortletParameters(PortletRequest portletRequest) {
+
+		if (portletRequest instanceof ResourceRequest) {
+			return ((ResourceRequest) portletRequest).getResourceParameters();
+		}
+		else {
+			return portletRequest.getRenderParameters();
+		}
 	}
 
 	private static class DeferredBridgeEventHandler extends BridgeEventHandlerWrapper {