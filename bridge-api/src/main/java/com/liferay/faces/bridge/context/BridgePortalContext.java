b68787fd5d6061b3135655cc5f1efc6b833d706e
Kyle Stiemann <stiemannkj1@gmail.com>
2017-03-28 15:04:29 -0400

    FACES-2974 Some children of jsf <head> components are not written to the response

--- bridge-api/src/main/java/com/liferay/faces/bridge/context/BridgePortalContext.java
+++ bridge-api/src/main/java/com/liferay/faces/bridge/context/BridgePortalContext.java
@@ -21,9 +21,18 @@ package com.liferay.faces.bridge.context;
 public final class BridgePortalContext {
 
 	/**
+	 * Property indicating if the portal application supports adding elements to the &lt;head&gt;...&lt;/head&gt;
+	 * section of the rendered portal page. A non-null value indicates that the portal application provides support.
+	 * @since 4.1.0
+	 */
+	public static final String ADD_ELEMENT_TO_HEAD_SUPPORT =
+		"com.liferay.faces.bridge.add.element.to.head.support";
+
+	/**
 	 * Property indicating if the portal application supports adding a &lt;script src=".." /&gt; resource to the
 	 * &lt;head&gt;...&lt;/head&gt; section of the rendered portal page. A non-null value indicates that the portal
 	 * application provides support.
+	 * @since 4.0.0
 	 */
 	public static final String ADD_SCRIPT_RESOURCE_TO_HEAD_SUPPORT =
 		"com.liferay.faces.bridge.add.script.resource.to.head.support";
@@ -32,6 +41,7 @@ public final class BridgePortalContext {
 	 * Property indicating if the portal application supports adding &lt;script&gt; text to the
 	 * &lt;head&gt;...&lt;/head&gt; section of the rendered portal page. A non-null value indicates that the portal
 	 * application provides support.
+	 * @since 4.0.0
 	 */
 	public static final String ADD_SCRIPT_TEXT_TO_HEAD_SUPPORT =
 		"com.liferay.faces.bridge.add.script.text.to.head.support";
@@ -40,6 +50,7 @@ public final class BridgePortalContext {
 	 * Property indicating if the portal application supports adding stylesheet &lt;link&gt; tags to the
 	 * &lt;head&gt;...&lt;/head&gt; section of the rendered portal page. A non-null value indicates that the portal
 	 * application provides support.
+	 * @since 4.0.0
 	 */
 	public static final String ADD_STYLE_SHEET_RESOURCE_TO_HEAD_SUPPORT =
 		"com.liferay.faces.bridge.add.style.sheet.resource.to.head.support";
@@ -48,6 +59,7 @@ public final class BridgePortalContext {
 	 * Property indicating if the portal application supports adding &lt;style&gt; text to the
 	 * &lt;head&gt;...&lt;/head&gt; section of the rendered portal page. A non-null value indicates that the portal
 	 * application provides support.
+	 * @since 4.0.0
 	 */
 	public static final String ADD_STYLE_SHEET_TEXT_TO_HEAD_SUPPORT =
 		"com.liferay.faces.bridge.add.style.sheet.text.to.head.support";
@@ -57,6 +69,7 @@ public final class BridgePortalContext {
 	 * that the {@link javax.portlet.PortletRequest#ACTION_PHASE} originates from an HTTP POST request, and the {@link
 	 * javax.portlet.PortletRequest#RENDER_PHASE} is caused by a subsequent HTTP GET request. A non-null value indicates
 	 * that the portal application provides support.
+	 * @since 4.0.0
 	 */
 	public static final String POST_REDIRECT_GET_SUPPORT = "com.liferay.faces.bridge.post.redirect.get.support";
 
@@ -65,6 +78,7 @@ public final class BridgePortalContext {
 	 * javax.portlet.ResourceResponse#HTTP_STATUS_CODE} property on the {@link javax.portlet.ResourceResponse}. A
 	 * non-null value indicates that the portal application provides support. A non-null value indicates that the portal
 	 * application provides support.
+	 * @since 4.0.0
 	 */
 	public static final String SET_HTTP_STATUS_CODE_SUPPORT = "com.liferay.faces.bridge.set.http.status.code.support";
 
@@ -72,6 +86,7 @@ public final class BridgePortalContext {
 	 * Property indicating if the portal application supports setting the buffer size on its {@link
 	 * javax.portlet.ResourceResponse} implementation. A non-null value indicates that the portal application provides
 	 * support.
+	 * @since 4.0.0
 	 */
 	public static final String SET_RESOURCE_RESPONSE_BUFFER_SIZE_SUPPORT =
 		"com.liferay.faces.bridge.set.resource.response.buffer.size.support";
@@ -79,6 +94,7 @@ public final class BridgePortalContext {
 	/**
 	 * Property indicating if the portal application supports creation of a render URL during the {@link
 	 * javax.portlet.PortletRequest#ACTION_PHASE} of the portlet lifecycle.
+	 * @since 4.0.0
 	 */
 	public static final String CREATE_RENDER_URL_DURING_ACTION_PHASE_SUPPORT =
 		"com.liferay.faces.bridge.create.render.url.during.action.phase.support";
@@ -86,6 +102,7 @@ public final class BridgePortalContext {
 	/**
 	 * Property indicating if the portal application requires parameters to be namespaced. A non-null value indicates
 	 * that the portal application provides support.
+	 * @since 4.0.0
 	 */
 	public static final String STRICT_NAMESPACED_PARAMETERS_SUPPORT =
 		"com.liferay.faces.bridge.strict.namespaced.paramters.support";
