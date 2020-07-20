0e83c8e45e70a92cd91a97d72aab94700d9c6b85
Neil Griffin <neil.griffin.scm@gmail.com>
2020-07-20 17:24:04 -0400

    FACES-2532 Incorporate Semantic Versioning detection via the baseline goal of the bnd-baseline-maven-plugin

--- bridge-api/src/main/java/javax/portlet/faces/package-info.java
+++ bridge-api/src/main/java/javax/portlet/faces/package-info.java
@@ -13,5 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@org.osgi.annotation.versioning.Version("1.0.0")
+@org.osgi.annotation.versioning.Version("4.2.0")
 package javax.portlet.faces;
